d874fb58a23fdbb3260c663c2d2485986d799a4f
BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
2022-11-03 20:29:14 -0300

    Agregado de Corrutina para el Nivel 3
    Ahora los Generadores02 dispararán según un tiempo aleatorio determinado dentro de una corrutina
    
    Co-Authored-By: ChArLeS090800 <112178322+ChArLeS090800@users.noreply.github.com>
    Co-Authored-By: EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>

--- Assets/Scripts/Generator02Behaviour.cs
+++ Assets/Scripts/Generator02Behaviour.cs
@@ -8,29 +8,30 @@ using UnityEngine;
 public class Generator02Behaviour : MonoBehaviour
 {
     [SerializeField]
-    private GameObject disparo;
-    [SerializeField]
-    private float tiempoAleatorio;
-    private float velocidad=1f;
+    private GameObject disparo; //Obtención del disparo o la bala
+    private float tiempoAleatorio; //Variable que almacenará el tiempoAleatorio de disparos
+    private float velocidad = 2.5f; //Variable que almacena el valor de la velocidad a la que se moverán los generadores
 
     void Start()
     {
-        Invoke("GenerarDisparo", tiempoAleatorio);
+        StartCoroutine("GenerarDisparo", tiempoAleatorio); //Iniciamos la Corrutina
     }
 
     private void Update()
     {
-        if (transform.position.x > 38 || transform.position.x < 20)
+        if (transform.position.z > 38 || transform.position.z < 20) //Método If que nos permite controlar el movimiento en Z de los generadores
         {
-            velocidad *= -1;
+            velocidad *= -1; //Se cambia el valor de la velocidad
         }
-        transform.Translate(0, 0, velocidad * Time.deltaTime);
+        transform.Translate(0, 0, velocidad * Time.deltaTime); //Se le asigna la nueva velocidad que tendrán los generadores
     }
 
-    public void GenerarDisparo()
+    IEnumerator GenerarDisparo() //Método que permite usar corrutinas
     {
-        Instantiate(disparo, transform.position, transform.rotation);
-        tiempoAleatorio = Random.Range(2, 5);
-        Invoke("GenerarDisparo", tiempoAleatorio);
+        float tiempoAleatorio = Random.Range(4, 8); //Establecemos el tiempoAleatorio con un random
+        yield return new WaitForSeconds(tiempoAleatorio); //Se espera según el tiempoAleatorio
+        Instantiate(disparo, transform.position, transform.rotation); //Se instancia un nuevo disparo con la misma posicion y rotación del generador
+
+        StartCoroutine("GenerarDisparo", tiempoAleatorio); //Se vuelve a llamar al método de la corrutina
     }
 }
