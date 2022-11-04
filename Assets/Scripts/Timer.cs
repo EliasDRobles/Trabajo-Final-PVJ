77b596e09354d06d8c34fb868ca2dbd3cb8a3870
estradalautaro <estradalautaro3@gmail.com>
2022-11-04 16:05:24 -0300

    Cambio menor para leer codigo
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>
    Co-Authored-By: EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
    Co-Authored-By: ChArLeS090800 <112178322+ChArLeS090800@users.noreply.github.com>

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,25 +5,25 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private TMP_Text timerText;
-    [SerializeField, Tooltip("Tiempo Restante en Segundos")] private float timerTime;
-    private int minutes, seconds, cents;  
+    [SerializeField] private TMP_Text timerText; // Se crea la referencia para el inspector de texto que usara nuestro Temporizador
+    [SerializeField, Tooltip("Tiempo Restante en Segundos")] private float timerTime; // Permite inicializar la cantidad de tiempo quetendra nuestro Juego en el proyecto
+    private int minutes, seconds, cents;  // Se instancia las variables int de minutos, segundos y centesimas
 
     private void Update()
     {
-        timerTime -= Time.deltaTime;
+        timerTime -= Time.deltaTime; // Permite restar el temporizador que instanciamos en el Proyecto
 
-        if (timerTime < 0) timerTime = 0;
-
-        minutes = (int)(timerTime / 60f);
+        if (timerTime < 0) timerTime = 0; // Si se cumplen las siguientes condiciones del Temporizador
+        // Se calcula tanto minutos, segundos y centesimas
+        minutes = (int)(timerTime / 60f); 
         seconds = (int)(timerTime - minutes * 60f);
         cents = (int)((timerTime - (int)timerTime) * 100f);
 
-        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents); // Permite visualizar el textTimer en la pantalla del Jugador
         
-        if (timerTime == 0)
+        if (timerTime == 0) // Al cumplirse la siguiente condicion, el temporizador se detendra en 00:00
         {
-            //Evento de Tiempo
+            // Se destruye el temporizador 
             Destroy(this);
         }
     }
