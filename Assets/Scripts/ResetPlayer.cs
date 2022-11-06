ba6817b81ce65e9ff144c88c0492943e3db39ef0
EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
2022-11-06 01:12:11 -0300

    Agregado devolucion del premio
    Cuando el premio caiga a la lava volverá al lugar donde estaba originalmente.
    
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
    Co-Authored-By: ChArLeS090800 <112178322+ChArLeS090800@users.noreply.github.com>
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>

--- Assets/Scripts/ResetPlayer.cs
+++ Assets/Scripts/ResetPlayer.cs
@@ -13,23 +13,43 @@ public class ResetPlayer : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         PlayerMov player = GameObject.Find("Perrito").GetComponent<PlayerMov>();
+        RewardBehaviour reward = GameObject.Find("Premio").GetComponent<RewardBehaviour>();
         switch (nivel)
         {
             case 1:
-                player.ActualVida = player.ActualVida - daño;
-                other.transform.position = new Vector3(-50, 16, 15);
-                    
-        break;
+                if (other.CompareTag("Player"))
+                {
+                    player.transform.position = new Vector3(-50, 16, 15);
+                    player.ActualVida = player.ActualVida - daño;
+                }
+
+
+                if (other.CompareTag("Premio"))
+                {
+                    reward.transform.position = new Vector3((float)-55.61, (float)15.5, (float)44.3);
+                }
+                break;
             case 2:
-                other.transform.position = new Vector3(0, 17, 15);
-                player.ActualVida = player.ActualVida - daño;
+                if (other.CompareTag("Player"))
+                {
+                    player.transform.position = new Vector3(0, 17, 15);
+                    player.ActualVida = player.ActualVida - daño;
+                }
+                if (other.CompareTag("Premio"))
+                {
+                    reward.transform.position = new Vector3(9, (float)15.5, -4);
+                }
                 break;
             case 3:
-                other.transform.position = new Vector3(50, 16, 15);
-                player.ActualVida = player.ActualVida - daño;
-                break;
-            case 4:
-                other.transform.position = new Vector3(0, 2, 0);
+                if (other.CompareTag("Player"))
+                {
+                    player.transform.position = new Vector3(50, 16, 15);
+                    player.ActualVida = player.ActualVida - daño;
+                }
+                if (other.CompareTag("Premio"))
+                {
+                    reward.transform.position = new Vector3(6, (float)15.5, -44);
+                }
                 break;
         }
 
