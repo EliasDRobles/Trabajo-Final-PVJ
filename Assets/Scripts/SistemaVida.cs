37b89972469b9c0b1252c1449370624c80019667
estradalautaro <estradalautaro3@gmail.com>
2022-11-05 22:24:50 -0300

    Se agrego una BarraDeVida
    Se puede visualizar en el nivel 1 una barra de vida, falta que este pueda bajar mediante las caidas
    
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
    Co-Authored-By: ChArLeS090800 <112178322+ChArLeS090800@users.noreply.github.com>
    Co-Authored-By: EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>

--- Assets/Scripts/SistemaVida.cs
+++ Assets/Scripts/SistemaVida.cs
@@ -5,54 +5,15 @@ using UnityEngine.UI;
 
 public class SistemaVida : MonoBehaviour
 {
-    public float maxVida = 100.0f;
-    public float actualVida;
+    public Image sistemaVida;
 
-    public bool inmortal = false;
-    public float tiempoInmortal = 1.0f;
+    public float vidaActual;
 
-    public Text vidaText;
+    public float vidaMaxima;
 
-    private void Start()
+    void Update()
     {
-        actualVida = maxVida;
-    }
-
-    private void Update()
-    {
-        if (actualVida > maxVida)
-            actualVida = maxVida;
-
-        if (actualVida <= 0)
-            Muerte();
-
-        // UI
-        vidaText.text = actualVida.ToString();
-    }
-
-    public void QuitarVida(float daño)
-    {
-        if (inmortal) return;
-
-        actualVida -= daño;
-        StartCoroutine(TiempoInmortal());
-    }
-
-    public void DarVida(float vida)
-    {
-        actualVida += vida;
-    }
-
-    public void Muerte()
-    {
-        Destroy(this.gameObject);
-    }
-
-    IEnumerator TiempoInmortal()
-    {
-        inmortal = true;
-        yield return new WaitForSeconds(tiempoInmortal);
-        inmortal = false;
+        sistemaVida.fillAmount = vidaActual / vidaMaxima;
     }
 
 }
